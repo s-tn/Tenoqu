0da3530662355534d7bf95395d14eaa07ca5b832
Ayce B (GreenWorld) <81369743+GreenyDEV@users.noreply.github.com>
2022-02-09 23:40:10 +0000

    c

--- public/chunks/channelLoad.js
+++ public/chunks/channelLoad.js
@@ -3,6 +3,7 @@
 // Front-end by GreenWorld#0001 
 // Back-end by EnderKingJ#0001 
 // This website and it's function is completely closed source. Please do not attempt to release it's source.
+
 main.app.loadChannel = async () => {
   if (document.querySelector('.failed')) document.querySelector('.failed').remove()
   var type = 'public'
