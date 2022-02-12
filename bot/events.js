fe14efeefc29c7d18e276dce75b7bf5a6645236a
Ayce B (GreenWorld) <81369743+GreenyDEV@users.noreply.github.com>
2022-02-12 15:21:57 +0000

    e

--- bot/events.js
+++ bot/events.js
@@ -8,7 +8,10 @@ const Fs = require('fs')
 module.exports = function(app) {
   app.ws('/message-listener', (cli, req) => {
     cli.bot = 'true'
+    cli.previousMessage = null
     cli.on('message', (m) => {
+      if (cli.previousMessage==m) return
+      cli.previousMessage = m
       m = JSON.parse(m)
       var Data = JSON.parse(Fs.readFileSync('./data/messages.json'))
       var MessageData = Data[m.channel]
@@ -16,6 +19,7 @@ module.exports = function(app) {
       //delete m.channel
       MessageData.push(m)
       Fs.writeFileSync('./data/messages.json', JSON.stringify(Data))
+      //console.log('gi')
       app.wss.getWss().clients.forEach(cli => cli.id?cli.send(JSON.stringify({type: 'message',channel:m.channel,data:m})):null)
       cli.send(JSON.stringify({type: 'messagedone', id: m.id}))
     })
